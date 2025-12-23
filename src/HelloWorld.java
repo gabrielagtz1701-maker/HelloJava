3a5238cd0fc9c03d1a2652e7b4dbee3680d6aec4
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2025-12-23 15:15:21 -0600

    Comentarios

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,11 @@
+/*
+Clase 1 - Introducción y Hola Mundo (23/12/25)
+Cometarios en varias líneas
+ */
+
+// Hola mundo
+// Comentarios en una sola línea
+
 public class HelloWorld {
 
     public static void main(String[] args) {
