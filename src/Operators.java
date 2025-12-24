05c045bcd0d9f2db50835eb1a7851629f48adc2d
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2025-12-24 17:36:20 -0600

    Operadores Unarios

--- src/Operators.java
+++ src/Operators.java
@@ -66,5 +66,16 @@ public class Operators {
         System.out.println(!false);
 
         System.out.println(!(3 > 2) || 5 == 2);
+
+        // Unarios (Operan sobre un solo operando)
+
+        System.out.println("Valor de b: " + b);
+        System.out.println(+b); // Mantiene el símbolo
+        System.out.println(-b); // Cambia el símbolo
+        System.out.println(++b); // Incrementa el valor antes de imprimirlo
+        System.out.println(b++); // Imprime el valor y luego lo incrementa
+        System.out.println(--b); // Reduce el valor y luego lo imprime
+        System.out.println(b--); // Imprime el vaor y luego lo reduce
+
     }
 }
