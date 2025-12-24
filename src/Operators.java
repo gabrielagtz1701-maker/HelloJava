09f80050e5dcb70b069916ec84dab64251f6c296
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2025-12-23 19:38:58 -0600

    Operadores Aritméticos

---
+++ src/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+        //Operadores
+
+        // Permiten realizar operaciones
+
+        // Aritméticos (se utilizan para realizar operaciones matemáticas
+
+        var a = 5;
+        var b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b); // Modulo, lo que sobra de la división
+        
+    }
+}
