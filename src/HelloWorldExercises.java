d068d2564c0109ae613a2833c69a0455c02ea019
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2025-12-23 18:33:17 -0600

    Ejercicios_HelloWorld

---
+++ src/HelloWorldExercises.java
@@ -0,0 +1,46 @@
+public class HelloWorldExercises {
+
+    public static void main(String[] args) {
+        // 1. Imprime un mensaje que diga tu nombre en lugar de "Hola mundo!"
+        System.out.println("Gabriela");
+
+        // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola\nMundo");
+
+        // 3. Añade un cometario sobre lo que hace cada línea del programa.
+            //System.out.println imprime en una sola línea de texto y con \n imprime en varias líneas
+
+        // 4. Crea un comentario en varias líneas
+            /*
+            Este es un comentario
+            en varias líneas del programa
+             */
+
+        // 5. Imprime tu edad, tu color favorito y tu ciudad.
+        System.out.println("Tengo 27 años, mi color favorito es el Blanco y vivo en el Estado de México");
+
+        // 6. Explora los diferentes System.XXX.println(); más allá de "out"
+
+        System.err.println("Salida de error estándar"); // Pone el texto en rojo como si fuera un error
+        //System.console().writer().println(); // Impresión controlada en consola
+
+        // 7. Utiliza varios println para imprimir una frase.
+        System.out.println("Una");
+        System.out.println("Frase");
+
+        // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos)
+        System.out.println("   *****   ");
+        System.out.println("  *     *  ");
+        System.out.println(" *  o o  * ");
+        System.out.println(" *   ^   * ");
+        System.out.println(" *  ___/ * ");
+        System.out.println("  *     *  ");
+        System.out.println("   *****   ");
+
+        // 9. Intenta ejecutar el programa sin el método main y observa el error.
+            //No permite ejecutar el programa
+
+        // 10. Intenta cambiar el nombre del archivo a uno diferente de la clase y compilalo.
+            //Solicita poner el mismo nombre de la clase en el archivo
+    }
+}
