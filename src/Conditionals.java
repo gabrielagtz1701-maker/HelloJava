3bd495bf80dd230412fd22d2fb2f55ec7f09f7e8
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2026-01-16 20:23:13 -0600

    Condicionales

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,2 +1,20 @@
 public class Conditionals {
+    public static void main(String[] args) {
+
+        // Condicionales
+
+        var age = 37;
+
+        System.out.println(age >= 18);
+
+        if (age > 18) {
+            System.out.println("El usuario es mayor de edad");
+        } else if (age == 18) {
+            System.out.println("El usuario acaba de cumplir 18");
+        } else {
+            System.out.println("El usuario es menor de edad");
+        }
+
+
+    }
 }
