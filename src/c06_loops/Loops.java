2d6848179d2f295cd860c22ec78b247d3d16fb6f
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2026-04-12 19:35:51 -0600

    Bucles

---
+++ src/c06_loops/Loops.java
@@ -0,0 +1,16 @@
+package c06_loops;
+
+public class Loops {
+    // Loops o Bucles
+    /*
+    Permite ejecutar un mismo bloque de código varias veces
+     */
+     public static void main(String[] args) {
+
+         // Loops
+         System.out.println("Hello World");
+
+         // for
+
+     }
+}
