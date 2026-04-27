8333b78de854d250427883c055d8c024703a3143
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2026-04-26 22:25:19 -0600

    Clases y Objetos

---
+++ src/c08_oop/Classes.java
@@ -0,0 +1,42 @@
+package c08_oop;
+
+public class Classes {
+    // Clases
+    /*
+    Dentro se definen los objetos y sus carateísticas
+    Se espera que la clase sea el fichero contenedor
+
+    Forma de crear
+    Modificador/palabra reservada class/nombre de la clase
+
+    Se pueden almacenar atributos y comportamientos (métodos o funciones)
+    Comienzan en mayusculas
+
+    Atributos - Variables que definene el estado y las características del objeto
+    Método - Funciones que definen el comportamiento del objeto
+    Objeto - Instancia que tiene esos atibutos y que utilizan esos métodos
+    Contructor - Permite inicializar el objeto con valores desde su creación
+
+    Orden de creación:
+    - Atributos
+    - Constructores
+    - Métodos
+     */
+
+    public static void main(String[] args) {
+
+        // Llamar a una clase
+        var person = new Person("Gabriela", 28);
+
+        //person.name = "Gabriela";
+        //person.age = 28;
+
+        person.sayHello();
+
+        person.name = "Gabriela1_1";
+        System.out.println(person.name);
+
+        var person2 = new Person("Gabriela2", 18);
+        person2.sayHello();
+    }
+}
---
+++ src/c08_oop/Person.java
@@ -0,0 +1,19 @@
+package c08_oop;
+
+public class Person {
+
+    // Atributos
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Métodos
+    public void sayHello() {
+        System.out.println(String.format("Hola soy %s y tengo %d", name, age));
+    }
+}
