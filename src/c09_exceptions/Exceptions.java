c21929ce4cb2a4f887ab292bd93c37708cf2f20f
Gabriela Gutierrez <gabrielagtz1701@gmail.com>
2026-05-11 15:48:28 -0600

    Manejo de Excepciones

--- src/c09_exceptions/Exceptions.java
+++ src/c09_exceptions/Exceptions.java
@@ -5,8 +5,48 @@ public class Exceptions {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        var result = 10 / 0;
-        System.out.println(result);
+
+        // Manejo de excepciones
+
+        // try catch
+        try {
+
+            // Colocar código que potencialmente es suceptible a fallar
+            var result = 10 / 0;
+            System.out.println(result);
+
+        } catch (Exception e) {
+            // Tipo de error a capturar
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+        // try con múltiples catch
+        try {
+            var result = 10/5;
+            System.out.println(result);
+
+            var name = "Gabriela";
+            name = null;
+            System.out.println("Name: " + name.toUpperCase());
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error aritmético: " + e);
+        } catch (NullPointerException e) {
+            System.out.println("Error null pointer");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error");
+        }
+
+        // finally
+        try {
+            var result = 10/0;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e);
+        } finally {
+            System.out.println("Fin del bloque try-catch");
+        }
 
         System.out.println("Fin");
     }
